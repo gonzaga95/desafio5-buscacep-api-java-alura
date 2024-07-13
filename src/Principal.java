1c1270c9eb9a1444a6b934296c10ea06ca724b67
gonzaga95 <gonzagas.dudu@gmail.com>
2024-07-12 21:32:10 -0400

    remove while do menu, add geradorjson

--- src/Principal.java
+++ src/Principal.java
@@ -1,25 +1,24 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Principal {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BuscadorCEP buscadorCEP = new BuscadorCEP();
         Scanner scanner = new Scanner(System.in);
         String cep;
 
-        while (true) {
+
+        try {
             System.out.println("Digite um CEP para busca: ");
             cep = scanner.nextLine();
-
-            if (cep.contains(("sair"))) break;
-
-            try {
-                Endereco novoEndereco = buscadorCEP.buscaEndereco(cep);
-
-                System.out.println(novoEndereco);
-            } catch (RuntimeException e) {
-                System.out.println(e.getMessage());
-                System.out.println("Tente novamente ou digite 'sair' ");
-            }
+            Endereco novoEndereco = buscadorCEP.buscaEndereco(cep);
+            System.out.println(novoEndereco);
+            GeradorJson geradorJson = new GeradorJson();
+            geradorJson.criaJson(novoEndereco);
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Tente novamente ou digite 'sair' ");
         }
+
     }
 }
